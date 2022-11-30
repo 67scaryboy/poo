54dbe376a13f03d6bdd64628565cf3ca01ec7a14
67scaryboy <67scaryboy@gmail.com>
2022-11-30 15:50:44 +0100

    definition carte

---
+++ catalogue.py
@@ -0,0 +1,41 @@
+""" A virer dès que plus besoin pour exemple
+from enum import Enum
+
+class Enom(Enum):
+    GEANT = "Géant"
+    MAGE_NOIR = "Mage noir"
+    GOBLIN = "Goblin"
+
+class Eatk(Enum):
+    GEANT = 3
+    MAGE_NOIR = 5
+    GOBLIN = 2
+
+class Evie(Enum):
+    GEANT = 3
+    MAGE_NOIR = 3
+    GOBLIN = 2
+
+class Eprix(Enum):
+    GEANT = 3
+    MAGE_NOIR = 4
+    GOBLIN = 2
+
+class Eeffet(Enum):
+    GEANT = 3
+    MAGE_NOIR = 3
+    GOBLIN = 2
+"""
+
+# [Nom,Attaque,Vie,Prix,Liste d'effet,race,tier]
+Cartes= [
+    ["Geant",3,3,3,[True,False,False,False,False],1,1],
+    ["Mage noir",5,3,4,[False,False,False,False,True],1,2]
+]
+# Liste d'effet:
+# [False, False, False, False, True]
+# Provocation: non
+# Bouclier divin: non
+# Toxicité: non
+# Furie des vents: non
+# Cri de guerre: oui
