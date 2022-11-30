937edd7cf2dbe4d14300c8b866e9e4790eeaa081
Centaure111 <louiscoutard@hotmail.fr>
2022-11-30 14:53:52 +0100

    modif main

--- Cmain.py
+++ Cmain.py
@@ -1,8 +1,8 @@
 import Ccarte
 
 class Main:
-    def __init__(self,cartesdispo,ormax,thune): # cartesdispo est une liste d'objets "Cartes",
-        self.NBCARTES= len(cartesdispo)
-        self.ORMAX = ormax #Or max possible d'avoir dans la main
-        self.OR = thune #Or possédé
-        self.CARTESDISPO = cartesdispo #Liste d'objet "cartes"
+    def __init__(self,p_cartesdispo,p_argentmax,p_argent): # cartesdispo est une liste d'objets "Cartes",
+        self.__nbcartes= len(p_cartesdispo)
+        self.__argentmax = p_argentmax #Or max possible d'avoir dans la main
+        self.__argent = p_argent #Or possédé
+        self.__cartesdispo = p_cartesdispo #Liste d'objet "cartes"
