eb6ee5067426774af931bf8f33af4e2d6d068aa9
Centaure111 <louiscoutard@hotmail.fr>
2022-11-30 15:59:44 +0100

    commentaires joueur

--- Cjoueur.py
+++ Cjoueur.py
@@ -4,14 +4,14 @@ class Joueur:
     def __init__(self, p_cartesdispo, p_argentmax, p_argent, p_pseudo):
         self.__pvmax = 20
         self.__pv = self.__pvmax
-        self.__main = Cmain.Main(p_cartesdispo)
-        self.__argentmax = p_argentmax
-        self.__argent = p_argent
-        self.__pseudo = p_pseudo
-        self.__boutique = Cboutique.Boutique()
-        self.__combatants = []
+        self.__main = Cmain.Main(p_cartesdispo) #Main du joueur
+        self.__argentmax = p_argentmax #Pièces max disponibles (dépassable, uniquement pour le compte des tours)
+        self.__argent = p_argent #Pièces disponibles
+        self.__pseudo = p_pseudo #Nom du joueur
+        self.__boutique = Cboutique.Boutique() #Boutique (propre au joueur)
+        self.__combatants = [] #Cartes posées
 
-    def acheter(self, p_carte):
+    def acheter(self, p_carte):#Déplacer les vérif avant les opérations
         cartesboutiques = Cboutique.Boutique.get_cartes(self.__boutique)
         if p_carte in cartesboutiques:
             if self.__argent >= 3:
