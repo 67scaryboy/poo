5d0cdd8e983247eb871ac2be1d7be90d71790363
67scaryboy <67scaryboy@gmail.com>
2022-11-30 17:29:48 +0100

    Refresh boutique

--- Cboutique.py
+++ Cboutique.py
@@ -1,4 +1,4 @@
-import Ccarte
+import Ccarte, random, catalogue
 
 class Boutique:
     def __init__(self):
@@ -13,6 +13,35 @@ class Boutique:
         self.__cartes = p_cartes
 
     def raffraichir(self):
+        self.__cartes = []
+        for i in range (0,5,1):
+            if self.__tier == 1:
+                self.__cartes.append(catalogue.liste_tier_1[random.randint(0,len(catalogue.liste_tier_1))])
+            elif self.__tier == 2:
+                if random.randint(0,1) == 0:
+                    self.__cartes.append(catalogue.liste_tier_1[random.randint(0,len(catalogue.liste_tier_1))])
+                else:
+                    self.__cartes.append(catalogue.liste_tier_2[random.randint(0,len(catalogue.liste_tier_2))])
+            elif self.__tier == 3:
+                temp=random.randint(0,2)
+                if temp == 0:
+                    self.__cartes.append(catalogue.liste_tier_1[random.randint(0,len(catalogue.liste_tier_1))])
+                elif temp == 1 :
+                    self.__cartes.append(catalogue.liste_tier_2[random.randint(0,len(catalogue.liste_tier_2))])
+                else:
+                    self.__cartes.append(catalogue.liste_tier_3[random.randint(0,len(catalogue.liste_tier_3))])
+            elif self.__tier == 4:
+                temp=random.randint(0,3)
+                if temp == 0:
+                    self.__cartes.append(catalogue.liste_tier_1[random.randint(0,len(catalogue.liste_tier_1))])
+                elif temp == 1 :
+                    self.__cartes.append(catalogue.liste_tier_2[random.randint(0,len(catalogue.liste_tier_2))])
+                elif temp == 2:
+                    self.__cartes.append(catalogue.liste_tier_3[random.randint(0,len(catalogue.liste_tier_3))])
+                else: 
+                    self.__cartes.append(catalogue.liste_tier_4[random.randint(0,len(catalogue.liste_tier_4))])
+
+
         listecartes = [] #choisir aléatoirement les cartes des tiers
         self.__cartes = listecartes
     
