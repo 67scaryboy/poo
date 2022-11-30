1aa740ac1c2666d0df8a73f947ab50b29ef72367
67scaryboy <67scaryboy@gmail.com>
2022-11-30 09:03:28 +0100

    ajout pvmax et réglage bugs

--- classes.py
+++ classes.py
@@ -2,22 +2,23 @@ import pygame, sys, random
 from pygame.locals import *
 
 class Cartes(pygame.sprite.Sprite):
-    def __init__(self): #Ajouter les paramètres ici. Plusieurs init possible -> Constructeur
+    def __init__(self,pvmax,atk,prix,effet,race,tier,position):
         super().__init__()
         self.image = pygame.image.load("sprites/e3.png") #A modifier au moment de l'interface graphique
-        self.PV=0 #PV du personnage. Pas maxé, on peut les monter au besoin (soins, boost ...)
-        self.ATK=0 #Attaque du perso
-        self.PRIX=0 #Prix d'achat en or
-        self.EFFET=0 #Effet particulier
-        self.RACE=0 #Race
-        self.TIER=0 #Tier de la boutique dans lequel il est achetable
-        self.POSITION=0 #Position pour affichage: 0=dans la boutique, 1= en main, 2=sur le terrain
+        self.PVMAX=pvmax #PV max de base
+        self.PV=pvmax #PV du personnage.
+        self.ATK=atk #Attaque du perso
+        self.PRIX=prix #Prix d'achat en or
+        self.EFFET=effet #Effet particulier
+        self.RACE=race #Race
+        self.TIER=tier #Tier de la boutique dans lequel il est achetable
+        self.POSITION=position #Position pour affichage: 0=dans la boutique, 1= en main, 2=sur le terrain
 
     def Attaque(self, adversaire): #Fait attaquer cette carte
         adversaire.PV-=self.ATK #L'adversaire prend les dégats
         self.PV-=adversaire.ATK #L'attaquant prend les dégats aussi
 
-    def Meurt(self) #Fait mourir la carte (La retire du terain)
+    def Meurt(self): #Fait mourir la carte (La retire du terain)
         if self.PV<1:
             #A faire
     
@@ -29,12 +30,12 @@ class Cartes(pygame.sprite.Sprite):
         #A faire
 
 class Main(pygame.sprite.Sprite):
-    def __init__(self,cartesdispo,ormax,or): # cartesdispo est une liste d'objets "Cartes",    
+    def __init__(self,cartesdispo,ormax,thune): # cartesdispo est une liste d'objets "Cartes",    
         super().__init__()
         self.image = pygame.image.load("sprites/e3.png")
         self.NBCARTES= len(cartesdispo)
         self.ORMAX = ormax #Or max possible d'avoir dans la main
-        self.OR = or #Or possédé
+        self.OR = thune #Or possédé
         self.CARTESDISPO = cartesdispo #Liste d'objet "cartes"
 
 class Terrain(pygame.sprite.Sprite):
