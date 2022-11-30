c37ba00c51593f1886f3edde8252b9d920782607
Centaure111 <louiscoutard@hotmail.fr>
2022-11-30 09:15:10 +0100

    la main et la boutique ne sont pas des sprites

--- classes.py
+++ classes.py
@@ -2,21 +2,21 @@ import pygame, sys, random
 from pygame.locals import *
 
 class Cartes(pygame.sprite.Sprite):
-    def __init__(self,pvmax,atk,prix,effet,race,tier,position):
+    def __init__(self,p_pvmax,p_atk,p_prix,p_effet,p_race,p_tier,p_position):
         super().__init__()
         self.image = pygame.image.load("sprites/e3.png") #A modifier au moment de l'interface graphique
-        self.PVMAX=pvmax #PV max de base
-        self.PV=pvmax #PV du personnage.
-        self.ATK=atk #Attaque du perso
-        self.PRIX=prix #Prix d'achat en or
-        self.EFFET=effet #Effet particulier
-        self.RACE=race #Race
-        self.TIER=tier #Tier de la boutique dans lequel il est achetable
-        self.POSITION=position #Position pour affichage: 0=dans la boutique, 1= en main, 2=sur le terrain
+        self.PVMAX=p_pvmax #PV max de base
+        self.PV=p_pvmax #PV du personnage.
+        self.ATK=p_atk #Attaque du perso
+        self.PRIX=p_prix #Prix d'achat en or
+        self.EFFET=p_effet #Effet particulier
+        self.RACE=p_race #Race
+        self.TIER=p_tier #Tier de la boutique dans lequel il est achetable
+        self.POSITION=p_position #Position pour affichage: 0=dans la boutique, 1= en main, 2=sur le terrain
 
-    def Attaque(self, adversaire): #Fait attaquer cette carte
-        adversaire.PV-=self.ATK #L'adversaire prend les dégats
-        self.PV-=adversaire.ATK #L'attaquant prend les dégats aussi
+    def Attaque(self, p_adversaire): #Fait attaquer cette carte
+        p_adversaire.PV-=self.ATK #L'adversaire prend les dégats
+        self.PV-=p_adversaire.ATK #L'attaquant prend les dégats aussi
 
     def Meurt(self): #Fait mourir la carte (La retire du terain)
         if self.PV<1:
@@ -29,10 +29,8 @@ class Cartes(pygame.sprite.Sprite):
     def Poser(self):
         #A faire
 
-class Main(pygame.sprite.Sprite):
-    def __init__(self,cartesdispo,ormax,thune): # cartesdispo est une liste d'objets "Cartes",    
-        super().__init__()
-        self.image = pygame.image.load("sprites/e3.png")
+class Main:
+    def __init__(self,cartesdispo,ormax,thune): # cartesdispo est une liste d'objets "Cartes",
         self.NBCARTES= len(cartesdispo)
         self.ORMAX = ormax #Or max possible d'avoir dans la main
         self.OR = thune #Or possédé
@@ -50,7 +48,6 @@ class Terrain(pygame.sprite.Sprite):
     def TourSuivant():
         #A faire
 
-class Boutique(pygame.sprite.Sprite):
+class Boutique:
     def __init__(self):
-        super().__init__()
         
