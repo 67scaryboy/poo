bcfc666e840d70c1296cbcb2364b4f2611c2399b
Max Descomps <max.descomps33@gmail.com>
2022-12-01 14:06:01 +0100

    -nomage des effets

--- champ_de_bataille.py
+++ champ_de_bataille.py
@@ -1,6 +1,6 @@
 import joueur, carte
-
 import random, math
+from catalogue import *
 
 class Champ_de_bataille():
     def __init__(self, joueur, ia):
@@ -10,14 +10,14 @@ class Champ_de_bataille():
     def LancerCombat(self):
         team_j = list(self.__joueur.GetCombatants())
         team_ia = list(self.__ia.GetCombatants())
-        relais = random.randint(0,1)
+        tour_du_joueur = random.randint(0,1)
         attaquant_j = 0
         attaquant_ia = 0
-        while (len(team_j) > 0) and (len(team_ia) > 0):
+        while (team_j and team_ia):
             place = -1
-            if relais == 1: #tour du joueur
+            if tour_du_joueur == True: #tour du joueur
                 for i in range(0,len(team_ia)):
-                    if team_ia[i].GetEffet()[0] == True: #si la carte a provocation
+                    if team_ia[i].GetEffet()['provocation'] == True: # la carte a provocation
                         place = i #elle devient la cible prioritaire
                 if place == -1: #si aucune carte n'a provocation
                     place = random.randint(0,len(team_ia) -1) #une carte est choisie aléatoirement
@@ -40,7 +40,7 @@ class Champ_de_bataille():
             
             else: #tour de l'ia
                 for j in range(0,len(team_j)):
-                    if team_j[j].GetEffet()[0] == True: #si la carte a provocation
+                    if team_j[j].GetEffet()['provocation'] == True: #si la carte a provocation
                         place = j #elle devient la cible prioritaire
                 if place == -1: #si aucune carte n'a provocation
                     place = random.randint(0,len(team_j) -1) #une carte est choisie aléatoirement
@@ -60,7 +60,8 @@ class Champ_de_bataille():
                     elif place == attaquant_j:
                         if place >= len(team_j):
                             attaquant_ia = 0
-            relais = relais + 1 % 2 #Changement de joueur
+
+            tour_du_joueur = tour_du_joueur + 1 % 2 #Changement de joueur
             
             #Affichage zbeule, a faire propre
             print("\n---===[COMBAT]===---")
@@ -72,6 +73,7 @@ class Champ_de_bataille():
                 mobs.Afficher()
 
         degats = 0
+        
         if len(team_j) > 0: #victoire joueur
             for carte in team_j:
                 degats += carte.GetAtkCombat()
