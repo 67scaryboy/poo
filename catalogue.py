1b7907fb4e9f45a6d61703085d87bcc0b35bf130
67scaryboy <67scaryboy@gmail.com>
2022-12-01 10:48:42 +0100

    Ajout enum Race

--- catalogue.py
+++ catalogue.py
@@ -16,40 +16,30 @@ def UpdateTierList(): #Classe le catalogue dans les tiers lists
             print ("La carte ", carte, " du catalogue appartient a un tier non définis (<1 ou >4)")
             exit(1)
 
-class Nom(Enum):
-    GEANT = 0
-    MAGE_NOIR = 1
-    GOBELIN = 2
-    LOUP = 3
-    FEUFOLET = 4
-    GARDE = 5
-    ASSASSIN = 6
-    PRETRE = 7
-    FANTOME = 8
-    TORTUEGEANTE = 9
-    DRAGON = 10
-    HEROS = 11
-    ROIDEMON = 12
-    VIRUS = 13
+class Races(Enum):
+    NONDEFINIS=0
+    MONSTRES=1
+    ANIMEAUX=2
+    HUMAINS=3
 
 #Listes de cartes par tier
 
 #id, nom, pv, atk, effet, race, tier
 catalogue = [
 Carte(0, "Geant", 2, 2, [True,False,False,False,False], 1, 1),
-Carte(1, "Mage noir", 2, 5,[False,False,False,False,True], 1, 2),
+Carte(1, "Mage noir", 2, 5,[False,False,False,False,True], 3, 2),
 Carte(2, "Gobelin", 3, 2,[False,False,False,False,True], 1, 1),
-Carte(3, "Loup", 2, 3,[False,False,False,False,False], 1, 1),
+Carte(3, "Loup", 2, 3,[False,False,False,False,False], 2, 1),
 Carte(4, "Feu follet", 4, 1,[False,False,False,False,False], 1, 1),
-Carte(5, "Garde de la ville", 4, 3,[True,False,False,False,False], 1, 2),
-Carte(6, "Assassin", 2, 1,[False,False,True,False,False], 1, 2),
-Carte(7, "Haut prêtre", 5, 4,[False,True,False,False,False], 1, 3),
+Carte(5, "Garde de la ville", 4, 3,[True,False,False,False,False], 3, 2),
+Carte(6, "Assassin", 2, 1,[False,False,True,False,False], 3, 2),
+Carte(7, "Haut prêtre", 5, 4,[False,True,False,False,False], 3, 3),
 Carte(8, "Fantome", 4, 4,[False,False,False,True,False], 1, 3),
-Carte(9, "Tortue géante", 7, 2,[True,False,False,False,True], 1, 3),
+Carte(9, "Tortue géante", 7, 2,[True,False,False,False,True], 2, 3),
 Carte(10, "Dragon sauvage", 6, 7,[False,False,False,False,False], 1, 4),
-Carte(11, "Héros", 7, 5,[False,True,False,False,True], 1, 4),
-Carte(12, "Roi démon", 8, 8,[True,False,False,False,True], 1, 4),
-Carte(13, "Virus", 1, 1,[False,True,True,True,False], 1, 4),
+Carte(11, "Héros", 7, 5,[False,True,False,False,True], 3, 4),
+Carte(12, "Roi démon", 8, 8,[True,False,False,False,True], 2, 4),
+Carte(13, "Virus", 1, 1,[False,True,True,True,False], 0, 4),
 ]
 # Liste d'effet:
 # [False, False, False, False, True]
