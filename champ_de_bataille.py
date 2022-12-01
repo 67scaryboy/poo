683c75a799faf59ea9d129a3135e95fd4350460f
Centaure111 <louiscoutard@hotmail.fr>
2022-12-01 09:45:25 +0100

    correction

--- champ_de_bataille.py
+++ champ_de_bataille.py
@@ -4,12 +4,14 @@ import random
 
 class Champ_de_bataille():
     def __init__(self, joueur, ia):
-        self.__joueur = p_joueur
-        self.__ia = p_ia
+        self.__joueur = joueur
+        self.__ia = ia
     
-    def LancerCombat():
+    def LancerCombat(self):
         team_j = self.__joueur.GetCombatants()
         team_ia = self.__ia.GetCombatants()
+        place_provoc_j = []
+        place_provoc_ia
         first = random.randint(0,1)
         while (len(team_j) > 0) and (len(team_ia) > 0):
             if first == 1: #le joueur commence
