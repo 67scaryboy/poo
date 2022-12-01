b40689350618695384629472645879d76441e5bd
67scaryboy <67scaryboy@gmail.com>
2022-12-01 09:44:00 +0100

    Debut ajout ia

--- jeu.py
+++ jeu.py
@@ -1,12 +1,17 @@
 from joueur import *
+from champ_de_bataille import *
 import catalogue
 
+
 def Principal():
-    j1 = Joueur(3, 3, "bob")
-    j1.AffStats()
+    j1 = Joueur(3, 4, "bob") #Initialise le joueur avec 1 d'or de plus que le max pour la première initialisation boutique
+    ia = Joueur(3, 4, "IA") #Pareil, mais avec l'ia
+    terrain = Champ_de_bataille(j1,ia)
 
     catalogue.UpdateTierList() #initialise les listes de cartes
     j1.RafraichirBoutique()
+    ia.RafraichirBoutique()
+    j1.AffStats()
 
     for carte in j1.GetBoutique().GetCartes():
         carte.Afficher()
